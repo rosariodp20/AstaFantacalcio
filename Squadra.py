31d4e7efce6692858c6080bac36e6144683a91cf
LUrbinati <s254074@studenti.polito.it>
2021-03-23 17:57:34 +0100

    Update Squadra.py

--- Squadra.py
+++ Squadra.py
@@ -1,7 +1,7 @@
 from Giocatore import Giocatore
 
 class Squadra:
-    
+
     def __init__(self, giocatore:Giocatore, nome:str, listaPortieri:list, listaDifensori:list,listaCentrocampisti:list,listaAttaccanti:list):
         self.giocatore=giocatore
         self.nome=nome
@@ -9,7 +9,7 @@ class Squadra:
         self.listaDifensori=listaDifensori
         self.listaCentrocampisti=listaCentrocampisti
         self.listaAttaccanti=listaAttaccanti
-    
+
     def getNomeGiocatore(self):
         return self.giocatore.getNome()
 
@@ -26,24 +26,36 @@ class Squadra:
     def getNumeroDifensori(self):
         num=len(self.listaDifensori)
         return num
-    
+
     def getNumeroCentrocampisti(self):
         num=len(self.listaCentrocampisti)
         return num
-    
+
     def getNumeroAttaccanti(self):
         num=len(self.listaAttaccanti)
         return num
-    
+
+    def getListaPortieri(self):
+        return self.listaPortieri
+
+    def getListaDifensori(self):
+        return self.listaDifensori
+
+    def getListaCentrocampisti(self):
+        return self.listaCentrocampisti
+
+    def getListaAttaccanti(self):
+        return self.listaAttaccanti
+
     def aggiungiPortiere(self, giocatore):
         self.listaPortieri.append(giocatore)
 
     def aggiungiDifensore(self, giocatore):
         self.listaDifensori.append(giocatore)
-    
+
     def aggiungiCentrocampista(self, giocatore):
         self.listaCentrocampisti.append(giocatore)
-    
+
     def aggiungiAttaccante(self, giocatore):
         self.listaAttaccanti.append(giocatore)
 
